10611d7099c6042c40f629d41c12d6df883c93e8
Maciej Jędrzejczak <mjedrzejczak@edu.cdv.pl>
2023-12-19 14:57:07 +0100

    // Previous update fix
    Character controller with working moving platforms, spawns rigidbody on player when needed. Rigidbody brakes dialogues with npc, probably trigger related. New bimba map for tram testing. New map needed, new models needed.

--- Assets/Scripts/StandOnPlatform.cs
+++ Assets/Scripts/StandOnPlatform.cs
@@ -1,22 +1,31 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class StandOnPlatform : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    [SerializeField] private GameObject player;
+
+    private void OnTriggerEnter(Collider player)
     {
-        if (other.CompareTag("Player"))
+        if (player.CompareTag("Player"))
         {
             // Set the player as a child of the platform
-            other.transform.parent = transform;
+            player.transform.parent = transform;
+            player.AddComponent<Rigidbody>();
+            player.GetComponent<Rigidbody>().freezeRotation = true;
+            Debug.Log("I am in");
         }
     }
 
-    private void OnTriggerExit(Collider other)
+    private void OnTriggerExit(Collider player)
     {
-        if (other.CompareTag("Player"))
+        if (player.CompareTag("Player"))
         {
             // Remove the player as a child of the platform
-            other.transform.parent = null;
+            player.transform.parent = null;
+            Destroy(player.GetComponent<Rigidbody>());
+
+            Debug.Log("I am out");
         }
     }
 }
