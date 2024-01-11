9d232cfa0eccb63d781876eddc13464eba71fb92
Ioculatorr <118347890+Ioculatorr@users.noreply.github.com>
2024-01-12 00:39:05 +0100

    // Lamp turn on delay

--- Assets/Scripts/Tests/Lamp.cs
+++ Assets/Scripts/Tests/Lamp.cs
@@ -6,14 +6,38 @@ public class Lamp : BaseWeapon
 {
 
     [SerializeField] private Light lampLight;
+
+    private bool isLightOn = false;
+    private bool canChangeLight = true;
     
     public override void Shoot()
     {
-        lampLight.intensity += 1;
+        if (!isLightOn && canChangeLight)
+        {
+            lampLight.enabled = true;
+            isLightOn = true;
+            canChangeLight = false;
+            StartCoroutine(LampWaitTime());
+        }
+        else if (isLightOn && canChangeLight)
+        {
+            lampLight.enabled = false;
+            isLightOn = false;
+            canChangeLight = false;
+            StartCoroutine(LampWaitTime());
+        }
     }
+
+    IEnumerator LampWaitTime()
+    {
+        yield return new WaitForSeconds(0.2f);
+        canChangeLight = true;
+    }
+
+
     void Update()
     {
-        
+        Debug.Log(canChangeLight);
     }
 
     public override void BulletEmit(Vector3 hitPoint)
