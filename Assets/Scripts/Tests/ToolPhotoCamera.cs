f2b4f3ad916fa588908e319b6d5ffd7b97bdff6f
Maciej Jędrzejczak <mjedrzejczak@edu.cdv.pl>
2024-01-18 23:06:52 +0100

    // PhotoCanvas war, prefab spawn fix?

--- Assets/Scripts/Tests/ToolPhotoCamera.cs
+++ Assets/Scripts/Tests/ToolPhotoCamera.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using UnityEngine.UI;
 
 public class ToolPhotoCamera : BaseWeapon
 {
@@ -11,14 +12,13 @@ public class ToolPhotoCamera : BaseWeapon
     [SerializeField] private AudioClip[] photoClips;
     [SerializeField] private Light photoLight;
     
-    [SerializeField] private GameObject targetPhoto;
     [SerializeField] private Transform photoAnimTarget;
-    
-    
-    
 
 
-    private Sprite snapshotSprite;
+
+    [SerializeField] private RawImage rawPhoto;
+    [SerializeField] private Canvas canvasPhoto;
+    
     private bool canTakePhoto = true;
     private bool canZoomPhoto = false;
 
@@ -40,34 +40,31 @@ public class ToolPhotoCamera : BaseWeapon
     
     void ReadPixelsFromRenderTexture(RenderTexture rt)
     {
-        targetPhoto.GetComponent<SpriteRenderer>().enabled = true;
-        // Create a Texture2D to read pixels into
-        Texture2D texture = new Texture2D(rt.width, rt.height, TextureFormat.RGB24, false);
 
-        // Set the target render texture
-        RenderTexture.active = rt;
-
-        // Read pixels from the render texture to the Texture2D
-        texture.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
-        texture.Apply();
+        // Create a new Texture2D
+        Texture2D texture2D = new Texture2D(renderTexture.width, renderTexture.height);
         
-        snapshotSprite = Sprite.Create(texture, new Rect(0.0f, 0.0f, texture.width, texture.height), new Vector2(0.5f, 0.5f), 100f);
-        targetPhoto.GetComponent<SpriteRenderer>().sprite = snapshotSprite;
-        PhotoPath();
-
-        // Reset the active render texture
+        // Read the pixels from the RenderTexture into the Texture2D
+        RenderTexture.active = renderTexture;
+        texture2D.ReadPixels(new Rect(0, 0, renderTexture.width, renderTexture.height), 0, 0);
+        texture2D.Apply();
         RenderTexture.active = null;
+
+        // Assign the captured frame to the RawImage
+        rawPhoto.texture = texture2D;
+
+        PhotoPath();
     }
 
     public override void AltShoot()
     {
         if (canZoomPhoto)
         {
-            targetPhoto.transform.DOLocalMove(new Vector3(-0.5f, 0.5f, -0.35f), 1f);
+            canvasPhoto.transform.DOLocalMove(new Vector3(-0.5f, 0.5f, -0.35f), 1f);
         }
         else
         {
-            targetPhoto.transform.DOLocalMove(new Vector3(-0.5f, -0.5f, 0.35f), 1f);
+            canvasPhoto.transform.DOLocalMove(new Vector3(-0.5f, -0.5f, 0.35f), 1f);
         }
 
         canZoomPhoto = !canZoomPhoto;
@@ -101,17 +98,17 @@ public class ToolPhotoCamera : BaseWeapon
 
     void PhotoPath()
     {
-        targetPhoto.transform.DOLocalMove(photoAnimTarget.localPosition, 1f);
+        canvasPhoto.transform.DOLocalMove(photoAnimTarget.localPosition, 1f);
     }
 
     void PhotoReset()
     {
-        targetPhoto.transform.localPosition = new Vector3(0f, 0f, 0f);
+        canvasPhoto.transform.localPosition = new Vector3(0f, 0f, 0f);
     }
     
     public override void DestroyModel()
     {
-        targetPhoto.GetComponent<SpriteRenderer>().enabled = false;
+        rawPhoto.texture = null;
         
         // Access spawnedPrefab from the BaseWeapon class
         base.DestroyModel();
