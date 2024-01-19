36e0c5f36c654ce39252c824e42148c4517cc63d
Ioculatorr <118347890+Ioculatorr@users.noreply.github.com>
2024-01-19 10:22:35 +0100

    // DOtween combine start
    loop, headbobbing started

--- Assets/Scripts/TweenCombiner.cs
+++ Assets/Scripts/TweenCombiner.cs
@@ -5,22 +5,53 @@ using DG.Tweening;
 
 public class TweenCombiner : MonoBehaviour
 {
+    [SerializeField] private float loopShakeDur = 1f;
+    [SerializeField] private float loopShakeStr = 3f;
+    [SerializeField] private int loopShakeVib = 10;
+    
+    public float bobbingAmount = 0.5f;
+    public float bobbingSpeed = 0.5f;
+
     void Start()
     {
+
         // Create your individual tweens
-        Tween tween1 = transform.DOMoveX(5f, 10f);
-        Tween tween2 = transform.DORotate(new Vector3(0f, 90f, 0f), 10f);
-        Tween tween3 = transform.DOScale(new Vector3(2f, 2f, 2f), 10f);
+        Tween loopTween = this.gameObject.transform.DOShakeRotation(loopShakeDur, loopShakeStr, loopShakeVib, 30f, false)
+            .SetLoops(-1, LoopType.Incremental);
+        
+        //Tween tween2 = transform.DORotate(new Vector3(0f, 90f, 0f), 10f);
+        //Tween tween3 = transform.DOScale(new Vector3(2f, 2f, 2f), 10f);
 
         // Combine tweens using the Join method
         Sequence combinedSequence = DOTween.Sequence();
-        combinedSequence.Append(tween1);
-        combinedSequence.Join(tween2);
-        combinedSequence.Join(tween3);
+        combinedSequence.Append(loopTween);
+        //combinedSequence.Join(tween2);
+        //combinedSequence.Join(tween3);
 
         // You can add more tweens using the Join method as needed
+        
+        // Add head bobbing to the combined sequence
+        //combinedSequence.AppendCallback(StartHeadBobbing);
 
         // Play the combined sequence
         combinedSequence.Play();
+    }
+    
+    private void StartHeadBobbing()
+    {
+        // Start head bobbing when called
+        StartCoroutine(HeadBob());
+    }
+
+    private IEnumerator HeadBob()
+    {
+        while (true)
+        {
+            // Move the head up
+            yield return this.transform.DOShakePosition(1f, 0.1f, 1, 15);
+
+            // Move the head down
+            yield return this.transform.DOShakePosition(1f, 0.1f, 1, 15);
+        }
     }
 }
