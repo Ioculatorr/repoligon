fff3fc6a134974fb77675d432bd5b88e4bbbf82f
Ioculatorr <118347890+Ioculatorr@users.noreply.github.com>
2024-03-05 22:16:51 +0100

    // Nie pamiętam co tu jest
    ale chce zabrać stąd charactercontroller do naszego horror projektu.

--- Assets/Scripts/Player/PlayerCam.cs
+++ Assets/Scripts/Player/PlayerCam.cs
@@ -11,11 +11,11 @@ public class PlayerCam : MonoBehaviour
     [SerializeField] private Transform orientation;
     [SerializeField] private Camera cmCamera;
     
-    // public float sensX = 400f;
-    // public float sensY = 400f;
-    //
-    // private float xRotation;
-    // private float yRotation;
+    public float sensX = 400f;
+    public float sensY = 400f;
+    
+    private float xRotation;
+    private float yRotation;
 
     private void Start()
     {
@@ -25,21 +25,21 @@ public class PlayerCam : MonoBehaviour
 
     // My own camera movement script, legacy because of cool Cinemachine POV feature.
     
-    private void FixedUpdate()
+    private void Update()
     {
-        // // Get mouse input
-        // float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
-        // float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
-        //
-        // yRotation += mouseX;
-        // xRotation -= mouseY;
-        //
-        // xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        // Get mouse input
+        float mouseX = Input.GetAxisRaw("Mouse X") * Time.deltaTime * sensX;
+        float mouseY = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * sensY;
         
-        // Rotate cam
-        // transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        yRotation += mouseX;
+        xRotation -= mouseY;
         
-        // Rotate orientation needed for movement
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        
+        //Rotate cam
+        transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+        
+        //Rotate orientation needed for movement
         
         orientation.rotation = Quaternion.Euler(0, cmCamera.transform.rotation.eulerAngles.y, 0);
     }
